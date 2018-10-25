20b9475c427fd416ec606656d9aa6450cff7d4f3
jponly <jpangeli514@gmail.com>
2018-10-25 15:04:55 -0700

    Updates

---
+++ precache-manifest.53da8f5b8e92ce98156b977991ff7630.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a759a28d702fb22a0dba",
+    "url": "/CLICKS/static/js/runtime~main.a759a28d.js"
+  },
+  {
+    "revision": "71f5618ce00263ed9403",
+    "url": "/CLICKS/static/js/main.71f5618c.chunk.js"
+  },
+  {
+    "revision": "6fea6cd249d4f943684f",
+    "url": "/CLICKS/static/js/1.6fea6cd2.chunk.js"
+  },
+  {
+    "revision": "71f5618ce00263ed9403",
+    "url": "/CLICKS/static/css/main.b4bb4321.chunk.css"
+  },
+  {
+    "revision": "655b49385046de5279856964434606ac",
+    "url": "/CLICKS/index.html"
+  }
+];
